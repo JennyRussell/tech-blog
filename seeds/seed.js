533cacf9c11b85a8af197694b5c4d2a39bc6eb71
Jenny Russell <moosyoolka@gmail.com>
2021-08-02 17:13:23 -0700

    added models folder and json seeds

---
+++ seeds/seed.js
@@ -0,0 +1,31 @@
+
+const blog = require("./blog.json");
+const users = require("./users.json");
+const sequelize = require("../config/connection");
+const User = require("../models/User");
+const Post = require("../models/BlogPost");
+
+
+const seedDatabase = async () => {
+  try {
+    await sequelize.sync({ force: true });
+    
+    await User.bulkCreate(users, {
+      individualHooks: true,
+      returning: true,
+    });
+    
+    await BlogPost.bulkCreate(blog, {
+      individualHooks: true,
+      returning: true,
+    });
+
+
+    process.exit(0);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+};
+
+seedDatabase();
